86f2bb4b50c62b3e9e7c90736b54add3c8f36d01
mdsabdul <mdsabdul088@gmail.com>
2024-07-03 15:29:34 +0530

    updated

--- models/messageschema.js
+++ models/messageschema.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
-const plm = require("passport-local-mongoose")
 
 const messageSchema = mongoose.Schema({
     receiver: String,
     sender: String,
     text: String
 })
-messageSchema.plugin(plm)
 
 module.exports = mongoose.model('message', messageSchema)
